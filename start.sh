8ece1e0c8562ec00fbe2033a213ac6dc37540a41
betterbot69 <110813024+betterbot69@users.noreply.github.com>
2022-12-13 03:47:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ echo "Cloning Repo...."
 git clone https://github.com/Greymattersbot/link-search-bot---mdisk-search-bot /Mdisk-Search-Bot
 cd /Mdisk-Search-Bot
 pip3 install -r requirements.txt
-echo "Starting Bot....GreyMatter_Bots"
-python3 main.py
+echo "Starting Bot....mbhbackup"
+python-3.8.7 main.py
