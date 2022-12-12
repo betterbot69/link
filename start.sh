5f3cdba444aa008cacb00d83f0a6f83daad0c17f
betterbot69 <110813024+betterbot69@users.noreply.github.com>
2022-12-13 03:51:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/Greymattersbot/link-search-bot---mdisk-search-bot /
 cd /Mdisk-Search-Bot
 pip3 install -r requirements.txt
 echo "Starting Bot....mbhbackup"
-python-3.8.7 main.py
+python3 main.py
